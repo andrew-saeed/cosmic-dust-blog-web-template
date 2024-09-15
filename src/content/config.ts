8256dbdde5c348cae512de426bc20fe197ef0c58
andrew-saeed <andrew-saeed.dev@outlook.com>
2024-09-15 00:00:00 +0000

    localize nav

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,10 +3,15 @@ import { z, defineCollection } from 'astro:content'
 const meta = defineCollection({
     type: 'data',
     schema: z.object({
+        "pathname": z.string(),
         "lang-picker": z.object({
             name: z.string(),
             link: z.string()
-        })
+        }),
+        "nav": z.array(z.object({
+            name: z.string(),
+            link: z.string()
+        }))
     })
 })
 
